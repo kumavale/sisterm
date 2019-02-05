d61a8ac2443d418bf30308edf678920beb5a3b5a
yorimoi <bhnk1270223@gn.iwasaki.ac.jp>
2019-02-05 16:24:48 +0900

    typo... Add palette.h

---
+++ palette.h
@@ -0,0 +1,16 @@
+#define COLOR_VENDORS      AQUA
+#define COLOR_COND         SILVER
+#define COLOR_KEYWORD      MAROON
+#define COLOR_PROTOCOL     OLIVE
+#define COLOR_STRING       YELLOW
+#define COLOR_INTERFACE    BLUE
+#define COLOR_ACTION       FUCHSIA
+#define COLOR_VAR          TEAL
+#define COLOR_IPV4_NET     RED
+#define COLOR_IPV4_SUB     PURPLE
+#define COLOR_IPV4_WILD    LIME
+//#define COLOR_IPV6
+//#define COLOR_CONFIGURE
+//#define COLOR_FUNCTION
+//#define COLOR_COMMENT
+//#define COLOR_SPACE
