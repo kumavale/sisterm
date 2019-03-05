62f2e387baaeaab14340c55c5df73d992dbb9b7c
yorimoi <bhnk1270223@gn.iwasaki.ac.jp>
2019-03-05 20:29:20 +0900

    Fixed bug

--- sist.bat
+++ sist.bat
@@ -3,8 +3,8 @@ TITLE sisterm
 FOR /L %%a in (1,1,20) do ( MODE COM%%a /STATUS | FINDSTR "COM.*:$" )
 echo.
 SET /P COM="COM port number: "
-wsl sudo chmod 666 /dev/ttyS%COM%
-wsl sist -l /dev/ttyS%COM%
+wsl sudo chmod 666 /dev/ttyS%COM%;^
+  if [ $? -eq 0 ];then sist -l /dev/ttyS%COM%; fi
 
 if not %ERRORLEVEL% == 0 (
   echo.
