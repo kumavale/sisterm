38d5f069ed6c921847af4fe925a30496a77e81e6
yorimoi <bhnk1270223@gn.iwasaki.ac.jp>
2019-03-05 22:41:17 +0900

    Stabled

--- sist.bat
+++ sist.bat
@@ -3,8 +3,7 @@ TITLE sisterm
 FOR /L %%a in (1,1,20) do ( MODE COM%%a /STATUS | FINDSTR "COM.*:$" )
 echo.
 SET /P COM="COM port number: "
-wsl sudo chmod 666 /dev/ttyS%COM%;^
-  if [ $? -eq 0 ];then sist -l /dev/ttyS%COM%; fi
+wsl sudo chmod 666 /dev/ttyS%COM%; sist -l /dev/ttyS%COM%
 
 if not %ERRORLEVEL% == 0 (
   echo.
