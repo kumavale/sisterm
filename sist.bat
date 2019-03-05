4c54c568e87cfa8dcf351d83e2d514938270d5b5
yorimoi <bhnk1270223@gn.iwasaki.ac.jp>
2019-03-05 20:08:40 +0900

    Tweak

--- sist.bat
+++ sist.bat
@@ -1,6 +1,6 @@
 @echo off
 TITLE sisterm
-FOR /L %%a in (1,1,9) do ( MODE COM%%a /STATUS | FINDSTR "COM.*:$" )
+FOR /L %%a in (1,1,20) do ( MODE COM%%a /STATUS | FINDSTR "COM.*:$" )
 echo.
 SET /P COM="COM port number: "
 wsl sudo chmod 666 /dev/ttyS%COM%
--- specificCOM.bat
+++ specificCOM.bat
@@ -1,2 +1,6 @@
 @echo off
-FOR /L %%a in (1,1,9) do ( MODE COM%%a /STATUS | FINDSTR "COM.*:$" )
+ECHO Processing...
+FOR /L %%a in (1,1,100) do ( MODE COM%%a /STATUS | FINDSTR "COM.*:$" )
+ECHO.
+ECHO Complete!
+SET /P _="Press any key..."
