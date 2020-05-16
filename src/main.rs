7d4a0906d9b402112a3a57bf57a639e792362b72
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-05-16 22:36:59 +0900

    I was able to read and write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+extern crate clap;
+extern crate serialport;
+
+use std::io::{self, Read, Write};
+use std::time::Duration;
+
+use clap::{App, AppSettings, Arg};
+use serialport::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Serialport Example - Receive Data")
+        .about("Reads data from a serial port and echoes it to stdout")
+        .setting(AppSettings::DisableVersion)
+        .arg(
+            Arg::with_name("port")
+                .help("The device path to a serial port")
+                .use_delimiter(false)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("baud")
+                .help("The baud rate to connect at")
+                .use_delimiter(false)
+                .required(true),
+        )
+        .get_matches();
+    let port_name = matches.value_of("port").unwrap();
+    let baud_rate = matches.value_of("baud").unwrap();
+
+    let mut settings: SerialPortSettings = Default::default();
+    settings.timeout = Duration::from_millis(10);
+    if let Ok(rate) = baud_rate.parse::<u32>() {
+        settings.baud_rate = rate.into();
+    } else {
+        eprintln!("Error: Invalid baud rate '{}' specified", baud_rate);
+        ::std::process::exit(1);
+    }
+
+    match serialport::open_with_settings(&port_name, &settings) {
+        Ok(mut port) => {
+            let mut serial_buf: Vec<u8> = vec![0; 1000];
+            let mut input_buffer = [0];
+            println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+            loop {
+                match port.read(serial_buf.as_mut_slice()) {
+                    Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+
+                match io::stdin().read(&mut input_buffer) {
+                    Ok(_) => {
+                        match port.write(&input_buffer) {
+                            Ok(_) => (),
+                            Err(e) => eprintln!("{}", e),
+                        }
+                    }
+                    Err(_) => (),
+                }
+
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+            ::std::process::exit(1);
+        }
+    }
 }
+
